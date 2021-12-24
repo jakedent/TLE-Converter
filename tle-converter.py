9f0aa9308596a5273f08e369677a0df7c94e93de
Jacob <info@jacobdent.com>
2021-12-24 13:35:06 +0000

    Update tle-converter.py

--- tle-converter.py
+++ tle-converter.py
@@ -1,11 +1,11 @@
-# TLE-Converter
+# TWO LINE ELEMENT SET TO LAT LON
 from __future__ import print_function
 import re
 import ephem
 
 
 def type_tle():
-    global tle_rec, type_name, type_line1, type_line2
+    global tle_rec
     type_name = "name"
     type_line1 = input("Enter Line 1 of TLE")
     type_line2 = input("Enter Line 2 of TLE")
@@ -19,29 +19,23 @@ def type_tle():
 
 
 def plot_lat_lon():
-    global target_line_1, target_line_2, lat, lon
     type_tle()
     try:
         lat = str(tle_rec.sublat) + '"E'
     except NameError as line_1_fail:
         print("TLE format error {0}.".format(line_1_fail))
     deg, minutes, seconds, direction = re.split('[::\'"]', lat)
-    # print((float(deg) + float(minutes) / 60 + float(seconds) / (60 * 60)) * (-1 if direction in ['W', 'S'] else 1))
     target_line_1 = (float(deg) + float(minutes) / 60 + float(seconds) / (60 * 60)) * (-1 if direction in ['W', 'S'] else 1)
     try:
         lon = str(tle_rec.sublong) + '"E'
     except NameError as line_2_fail:
         print("TLE format error {0}".format(line_2_fail))
     deg, minutes, seconds, direction = re.split('[::\'"]', lon)
-    # print((float(deg) + float(minutes) / 60 + float(seconds) / (60 * 60)) * (-1 if direction in ['W', 'S'] else 1))
     target_line_2 = (float(deg) + float(minutes) / 60 + float(seconds) / (60 * 60)) * (-1 if direction in ['W', 'S'] else 1)
-
-
-def plot_cords():
-    plot_lat_lon()
+    # PLOT THE RESULTS
     target_result = "Lat: " + str(target_line_1), "Lon: " + str(target_line_2)
     print("Converted:", str(target_result).strip("()"))
 
 
 if __name__ == '__main__':
-    plot_cords()
+    plot_lat_lon()
